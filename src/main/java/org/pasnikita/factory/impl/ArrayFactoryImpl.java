7e81a92b222685075b183c2ed4514e4b7c42786c
NikitaPas <nikita223372@gmail.com>
2025-11-20 22:16:41 +0300

    changes

--- src/main/java/org/pasnikita/factory/impl/ArrayFactoryImpl.java
+++ src/main/java/org/pasnikita/factory/impl/ArrayFactoryImpl.java
@@ -3,16 +3,15 @@ package org.pasnikita.factory.impl;
 import org.pasnikita.entity.CustomArray;
 import org.pasnikita.exception.ArrayException;
 import org.pasnikita.factory.ArrayFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class ArrayFactoryImpl implements ArrayFactory {
-
+  public static final Logger LOGGER = LogManager.getLogger();
   private static int counter = 1;
 
   @Override
   public CustomArray createArray(int[] array) throws ArrayException {
-    if (array == null || array.length == 0) {
-      throw new ArrayException("Array cannot be empty");
-    }
     return new CustomArray(counter++, array);
   }
 }
--- src/main/java/org/pasnikita/parser/impl/ArrayParserImpl.java
+++ src/main/java/org/pasnikita/parser/impl/ArrayParserImpl.java
@@ -1,26 +1,25 @@
 package org.pasnikita.parser.impl;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.pasnikita.exception.ArrayException;
 import org.pasnikita.parser.ArrayParser;
 
 import java.util.Arrays;
 
 public class ArrayParserImpl implements ArrayParser {
-
+  public static final Logger LOGGER = LogManager.getLogger();
   private static final String DELIMITER_REGEX = "[\\p{P}\\s]+";
 
   @Override
   public int[] parseLine(String line) throws ArrayException {
-
-    if (line == null) {
-      throw new ArrayException("Line cannot be null");
-    }
 
     try {
       return Arrays.stream(line.trim().split(DELIMITER_REGEX))
               .mapToInt(Integer::parseInt)
               .toArray();
     } catch (NumberFormatException e) {
+      LOGGER.error("Invalid number in line", e);
       throw new ArrayException("Invalid number in line: " + line, e);
     }
   }
